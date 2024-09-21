3f1cfe8ac9d96a7be9ddd8b9921d9c338d4867ca
HumHub Translations <hello@humhub.com>
2024-09-21 08:24:40 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt-BR/base.php
+++ messages/pt-BR/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    '<strong>Online</strong> users' => '',
-    'Dashboard sidebar order' => '',
-    'General settings for the Online Users module.' => '',
-    'Limit users on the dashboard widget' => '',
-    'Number of users' => '',
-    'Settings' => '',
-];
+return array (
+  '<strong>Online</strong> users' => 'Usuários <strong>online</strong>',
+  'Dashboard sidebar order' => 'Ordem da barra lateral do painel',
+  'General settings for the Online Users module.' => 'Configurações gerais para o módulo Usuários Online.',
+  'Limit users on the dashboard widget' => 'Limitar usuários no widget do painel',
+  'Number of users' => 'Número de usuários',
+  'Settings' => 'Configurações',
+);
--- messages/vi/base.php
+++ messages/vi/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    '<strong>Online</strong> users' => '',
-    'Dashboard sidebar order' => '',
-    'General settings for the Online Users module.' => '',
-    'Limit users on the dashboard widget' => '',
-    'Number of users' => '',
-    'Settings' => '',
-];
+return array (
+  '<strong>Online</strong> users' => '',
+  'Dashboard sidebar order' => '',
+  'General settings for the Online Users module.' => '',
+  'Limit users on the dashboard widget' => '',
+  'Number of users' => '',
+  'Settings' => 'Cài đặt',
+);

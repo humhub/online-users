0ce84d96cc5125e90e6f8866d735d30b4e3f7e5c
HumHub Translations <hello@humhub.com>
2025-04-19 17:32:24 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,9 +1,9 @@
 <?php
-return array (
-  '<strong>Online</strong> users' => '',
-  'Dashboard sidebar order' => '',
-  'General settings for the Online Users module.' => '',
-  'Limit users on the dashboard widget' => '',
-  'Number of users' => '',
+return [
+  '<strong>Online</strong> users' => '<strong>Online</strong> Nutzer',
+  'Dashboard sidebar order' => 'Seitenmenü Reihenfolge in der Übersicht',
+  'General settings for the Online Users module.' => 'Allgemeine Einstellungen für die Online-Nutzer Module.',
+  'Limit users on the dashboard widget' => 'Höchstmenge der Nutzer auf dem Übersichts-Widget',
+  'Number of users' => 'Anzahl der Nutzer',
   'Settings' => 'Einstellungen',
-);
+];
